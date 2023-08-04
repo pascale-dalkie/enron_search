3db146f770ee00b223ad7724a5c803b3243034c4
pascale-sushi <53155830+pascale-dalkie@users.noreply.github.com>
2023-08-04 20:39:22 +1000

    Addition of related results

--- app/services/message_search_service.rb
+++ app/services/message_search_service.rb
@@ -16,13 +16,14 @@ class MessageSearchService
     end
 
     #  Create a hash with the message ID as the key and the matched snippets as the value
-    formatted_result = search_result.hits.map do | result |
-      formatted_match = result.highlights.map { |highlight| highlight.format { |word| "**#{word}**"}}.join(", ")
-      [result.primary_key, formatted_match]
+    formatted_result = formatted_search_hash(search_result).to_h
+
+    # if the query has a sender and a date range then we add on what else they sent during that time
+    if sender && start_date && end_date
+      formatted_result["related_results"] = related_search
     end
 
-    # return in hash form
-    formatted_result.to_h
+    formatted_result
   end
 
   private
@@ -41,5 +42,16 @@ class MessageSearchService
 
   def end_date
     @end_date ||= @params["end_date"].to_datetime
+  end
+
+  def formatted_search_hash(search_result)
+    search_result.hits.map do | result |
+      formatted_match = result.highlights.map { |highlight| highlight.format { |word| "**#{word}**"}}.join(", ")
+      [result.primary_key, formatted_match]
+    end
+  end
+
+  def related_search
+    Message.where(sender: sender).where(date: start_date..end_date)
   end
 end
